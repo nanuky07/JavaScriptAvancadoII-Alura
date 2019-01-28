a71afbfd61252a2886067dd207d20ed620672000
Marcus Santos <marcusdcarli@gmail.com>
2019-01-28 07:01:53 -0200

    AUla 2

--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,16 +1,14 @@
 class ListaNegociacoes {
 
-    constructor(armadilha) {
+    constructor() {
 
         this._negociacoes = [];
-        this._armadilha = armadilha;
 
     }
 
     adiciona(negociacao) {
 
         this._negociacoes.push(negociacao);
-        this._armadilha(this);
 
     }
 
@@ -21,7 +19,6 @@ class ListaNegociacoes {
 
     esvazia() {
         this._negociacoes = [];
-        this._armadilha(this);
 
     }
 }
